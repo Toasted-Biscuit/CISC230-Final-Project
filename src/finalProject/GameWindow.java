a3cb7e5ec6340f0e591bd55511e23862bf7b2aed
KaySep-git <pream77327@gmail.com>
2026-05-07 10:42:10 -0500

    I changed the button

--- src/finalProject/GameWindow.java
+++ src/finalProject/GameWindow.java
@@ -12,6 +12,7 @@ public class GameWindow extends Application{
 	public void start(Stage ps) {
 		Label title = new Label("Game and stuff");
 		Button button = new Button("Click Me!");
+		Button nw = new Button("Hello");
 		button.setLayoutX(200);
 		button.setLayoutY(200);
 		
