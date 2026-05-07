7b4b69a0f77889807afa2c6369eee1d4ee499bc1
Toasted-Biscuit <nathan.hansing@outlook.com>
2026-05-07 15:06:32 -0500

    Added logic to check if there is a winner on the board

--- src/finalProject/Connect4.java
+++ src/finalProject/Connect4.java
@@ -4,9 +4,9 @@ package finalProject;
 public class Connect4 {
 	
 	// TODO consider making a chip class instead of numbers. Might be better or might not be
-	public final int EMPTY = 0;
-	public final int YELLOW = 1;
-	public final int RED = 2;
+	public static final int EMPTY = 0;
+	public static final int YELLOW = 1;
+	public static final int RED = 2;
 	
 	private int[][] board;
 	
@@ -18,7 +18,7 @@ public class Connect4 {
 	// returns -1 if the column is full
 	private int getAvailableIndex(int col) {
 		for (int i = board.length - 1; i >= 0; i--) {
-			if (board[i][col] == 0) {
+			if (board[i][col] == EMPTY) {
 				return i;
 			}
 		}
@@ -37,6 +37,68 @@ public class Connect4 {
 		}
 	}
 	
+	// Checks board from left to right and up to down. Finds the first instance of
+	// a 4 in a row pattern and returns the chip color of the winner. Returns 0 if there are no winners
+	public int checkWinner() {
+		for (int r = 0; r < board.length; r++) {
+			for (int c = 0; c < board[r].length; c++) {
+				if (board[r][c] != EMPTY) {
+					int result = findWinningPattern(r, c, board[r][c], 0, 5);
+					if (result != 0) {
+						return result;
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	// Helper method for checkWinner()
+	// takes a row, column, chip, chipInSequence, and direction.
+	/*
+	 * Recursively attempts to find a pattern of 4 chips in a row with the same color
+	 * row: The row that the method will check
+	 * col: The column that the method will check
+	 * chip: Defines the chip color that we are searching for
+	 * chipInSequence: The number of chips in a row that have been found. Stops after reaching 4
+	 * direction: Defines the direction on the board that the program is searching. When 5 is given, checks all directions for a pattern
+	 * 			  Starts at 4 because 1, 2, and 3 are not needed.
+	 * 			  The value of direction can be thought of like the numbers you dial on a phone.
+	 * 			  Imagine an arrow going from the middle to the number, that is the direction it searches:
+	 * 			  1 2 3
+	 * 			  4 * 6
+	 * 			  7 8 9
+	 */
+	private int findWinningPattern(int row, int col, int chip, int chipInSequence, int direction) {
+		if (chipInSequence == 4) {
+			return chip;
+		} else if (row == board.length || row < 0 ||  col == board[row].length || col < 0) {
+			return 0;
+		}
+		
+		if (board[row][col] == chip) {
+			if (direction == 5) {
+				for (int i = 4; i <= 9; i++) {
+					if (i != 5 && findWinningPattern(row, col, chip, chipInSequence, i) == chip) {
+						return chip;
+					}
+				}
+			} else if (direction == 4) {
+				return (findWinningPattern(row, --col, chip, ++chipInSequence, direction) == chip) ? chip:  0;			
+			} else if (direction == 6) {
+				return (findWinningPattern(row, ++col, chip, ++chipInSequence, direction) == chip) ? chip:  0;
+			} else if (direction == 7) {
+				return (findWinningPattern(++row, --col, chip, ++chipInSequence, direction) == chip) ? chip:  0;
+			} else if (direction == 8) {
+				return (findWinningPattern(++row, col, chip, ++chipInSequence, direction) == chip) ? chip:  0;
+			} else if (direction == 9) {
+				return (findWinningPattern(++row, ++col, chip, ++chipInSequence, direction) == chip) ? chip:  0;
+			}			
+		}
+		
+		return 0;
+	}
+	
 	// Displays the game board in the console. Meant for testing and debugging
 	public void displayBoard() {
 		System.out.println("Current Board:");
@@ -47,6 +109,5 @@ public class Connect4 {
 			}
 			System.out.println();
 		}
-	}
-	
+	}	
 }
--- src/finalProject/Connect4BoardExample.java
+++ src/finalProject/Connect4BoardExample.java
@@ -6,16 +6,75 @@ import java.util.Random;
 public class Connect4BoardExample {
 	public static void main(String[] args) {
 		Random rand = new Random();
-		Connect4 c4 = new Connect4();
-		c4.displayBoard();
-		c4.placeChip(0, c4.YELLOW);
-		c4.placeChip(0, c4.RED);
+		Connect4 c4 = new Connect4();		
+		
+//		placeLeftDiagonal(c4);
+//		placeRightDiagonal(c4);
+//		placeVertLine(c4);
+//		placeHorizLine(c4);
+		randomGame(c4, 20);
 		c4.displayBoard();
 		
-		for (int i = 0; i < 20; i++) {
-			c4.placeChip(rand.nextInt(0, 7), rand.nextInt(1, 3));
+		System.out.println(c4.checkWinner());
+		
+	}
+	
+	// Plays a game by placing chips in random columns. Stops when someone wins
+	public static void randomGame(Connect4 c4, int rounds) {
+		Random rand = new Random();
+		int turn = Connect4.YELLOW;
+		while (c4.checkWinner() == 0 && rounds > 0) {
+			c4.placeChip(rand.nextInt(0, 7), turn);
+			if (turn == Connect4.YELLOW) {
+				turn = Connect4.RED;
+			} else {
+				turn = Connect4.YELLOW;
+			}
+			rounds--;
 		}
-		
-		c4.displayBoard();
+	}
+	
+	// Places a pattern of chips in a left diagonal
+	public static void placeLeftDiagonal(Connect4 c4) {
+		c4.placeChip(2, Connect4.YELLOW);
+		c4.placeChip(1, Connect4.YELLOW);
+		c4.placeChip(1, Connect4.YELLOW);
+		c4.placeChip(0, Connect4.YELLOW);
+		c4.placeChip(0, Connect4.YELLOW);
+		c4.placeChip(0, Connect4.YELLOW);
+		c4.placeChip(0, Connect4.RED);
+		c4.placeChip(1, Connect4.RED);
+		c4.placeChip(2, Connect4.RED);
+		c4.placeChip(3, Connect4.RED);
+	}
+	
+	// Places a pattern of chips in a right diagonal
+	public static void placeRightDiagonal(Connect4 c4) {
+		c4.placeChip(1, Connect4.YELLOW);
+		c4.placeChip(2, Connect4.YELLOW);
+		c4.placeChip(2, Connect4.YELLOW);
+		c4.placeChip(3, Connect4.YELLOW);
+		c4.placeChip(3, Connect4.YELLOW);
+		c4.placeChip(3, Connect4.YELLOW);
+		c4.placeChip(0, Connect4.RED);
+		c4.placeChip(1, Connect4.RED);
+		c4.placeChip(2, Connect4.RED);
+		c4.placeChip(3, Connect4.RED);
+	}
+	
+	// Places a pattern of chips in a vertical line
+	public static void placeVertLine(Connect4 c4) {
+		c4.placeChip(0, Connect4.RED);
+		c4.placeChip(0, Connect4.RED);
+		c4.placeChip(0, Connect4.RED);
+		c4.placeChip(0, Connect4.RED);
+	}
+	
+	// Places a pattern of chips in a horizontal line
+	public static void placeHorizLine(Connect4 c4) {
+		c4.placeChip(0, Connect4.RED);
+		c4.placeChip(1, Connect4.RED);
+		c4.placeChip(2, Connect4.RED);
+		c4.placeChip(3, Connect4.RED);
 	}
 }
